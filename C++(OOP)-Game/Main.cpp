0457f7e1230b38b67ccd393042fb0b3e053a4074
Göktay Bahçekapılı <94232225+goktaybah@users.noreply.github.com>
2022-05-26 11:40:37 +0300

    Update Main.cpp

--- C++(OOP)-Game/Main.cpp
+++ C++(OOP)-Game/Main.cpp
@@ -1,13 +1,4 @@
-/*
-* Göktay
-Bahçekapýlý
-210757043
-Bilgisayar mühendisliði ikinci öðretim
-Nesneye yönelik programlama / BM104
-BAHAR 2022
-BM104
-DENÝZ DAL
-*/
+
 #include <iostream>
 #include "BM104_210757043_Proje1_Bahar.h"
 
@@ -27,7 +18,7 @@ int main()
 	cin >> altLimit;
 	cout << "Ust Limit:";
 	cin >> ustLimit;
-	//classýmýzý olusturuyoruz
+	//classÄ±mÄ±zÄ± olusturuyoruz
 	BM104_210757043_Proje1_Bahar nsn(kacEleman, altLimit, ustLimit);
 	nsn.OyunOyna();
 }
